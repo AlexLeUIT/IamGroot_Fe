08ca8d500c1ca887def965dc330965f0b222e60c
LeMinhKhanh <95294684+KaitoEight@users.noreply.github.com>
2025-07-08 23:55:22 +0700

    UI/UX Build

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div className=''>
+        {/* Logo */}
+        <div className=''>
+            <img src="/logo.png" alt="Logo" className='w-8 h-8'/>
+            <span>IamGroot</span>
+        </div>
+        {/* Mobile */}
+        <div className="md:hidden">
+            M
+        </div>
+        {/* Logo */}
+        {/* Logo */}
+    </div>
+  )
+}
+
+export default Navbar
