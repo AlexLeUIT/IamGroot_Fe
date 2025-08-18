697f6586213b3fe33a5ce6ceb3057169393d8c10
Alex Le <24560002@gm.uit.edu.vn>
2025-08-18 12:44:16 +0700

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,15 @@
 import Navbar from '../components/Navbar.jsx';
 import {Outlet} from 'react-router-dom';
-import Footer from '../components/Footer.jsx';
 const MainLayout = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
         <Navbar/>
         <Outlet/>
-        //<Footer/>
     </div>
   )
 }
 
 export default MainLayout
+
 
 
