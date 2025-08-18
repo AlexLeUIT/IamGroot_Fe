2b996cac6d9a13f81cca68a775425a38dc22ff9d
Alex Le <24560002@gm.uit.edu.vn>
2025-08-18 11:53:17 +0700

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,10 +6,11 @@ const MainLayout = () => {
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
         <Navbar/>
         <Outlet/>
-        <Footer/>
+        //<Footer/>
     </div>
   )
 }
 
 export default MainLayout
+
 
